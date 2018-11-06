2610f86a62399a84d50bb358fd89961537615a29
crunchycrust <43686079+crunchycrust@users.noreply.github.com>
2018-11-06 12:20:12 +0300

    Update solution.pl

--- logoprogo/solution.pl
+++ logoprogo/solution.pl
@@ -4,23 +4,18 @@
 %но ему сказали, что столяр работает в доме водопроводчика. 
 %Известно также, что Федоров никогда не слышал о Давыдове. Кто чем занимается? 
 
-prof(plumber).
+
 prof(carpenter).
 prof(painter).
+prof(plumber).
 
 knows(painter, carpenter).
 knows(carpenter, painter).
 
 knownot(painter, plumber).
 
-difcheck([Con, Dav, Fed]) :-
-	Con \== Dav,
-	Dav \== Fed,
-	Con \== Fed.	
-
 solution([Con, Dav, Fed]) :-
-	prof(Pr1), prof(Pr2), prof(Pr3),
+	prof(Pr1), prof(Pr2), Pr1 \== Pr2,
+	prof(Pr3), Pr2 \== Pr3, Pr1 \== Pr3,
 	Con = Pr1, Dav = Pr2, Fed = Pr3,
-	Aaa = [Con, Dav, Fed],
-	difcheck(Aaa),
 	knownot(Fed, Dav), not(knows(Fed, Dav)).
