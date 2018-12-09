a7c9b4f097e0c77b0ce77597508867e8c43e7311
crunchycrust <43686079+crunchycrust@users.noreply.github.com>
2018-12-09 23:33:19 +0300

    Add files via upload

---
+++ logoprogo/cp/pred.pl
@@ -0,0 +1,93 @@
+sex('Shakespeare William', m).
+sex('Arden Mary', f).
+sex('Shakespeare John', m).
+sex('Hathaway Anne', f).
+sex('Shakespeare Susanna', f).
+sex('Shakespeare Hamnet', m).
+sex('Shakespeare Judith', f).
+sex('Shakespeare Joan1', f).
+sex('Shakespeare Margaret', f).
+sex('Shakespeare Gilbert', m).
+sex('Shakespeare Joan2', f).
+sex('Shakespeare Anne', f).
+sex('Shakespeare Richard Jr.', m).
+sex('Shakespeare Edmund', m).
+sex('Shakespeare Richard', m).
+sex('Shakespeare Henry', m).
+sex('Hathaway Joan', f).
+sex('Webbe Agnes', f).
+sex('Arden Robert', m).
+sex('Hall John', m).
+sex('Shakespeare Elizabeth', f).
+sex('Nash Thomas', m).
+sex('Barnard John', m).
+sex('Quiney Thomas', m).
+sex('Quiney Shakespeare', m).
+sex('Quiney Richard', m).
+sex('Quiney Thomas Jr.', m).
+sex('Hathaway Richard', m).
+sex('Webb Abigail', f).
+sex('Shakespeare John Sr.', m).
+sex('Unknown Joan', f).
+sex('Hart William', m).
+sex('Hart William Jr.', m).
+sex('Hart Mary', f).
+sex('Hart Thomas', m).
+sex('Hart Michael', m).
+sex('Shakespeare Anna', f).
+sex('Shakespeare Thomas', m).
+sex('Shakespeare Matthew', m).
+parent('Shakespeare John', 'Shakespeare William').
+parent('Arden Mary', 'Shakespeare William').
+parent('Shakespeare John', 'Shakespeare Joan1').
+parent('Arden Mary', 'Shakespeare Joan1').
+parent('Shakespeare John', 'Shakespeare Margaret').
+parent('Arden Mary', 'Shakespeare Margaret').
+parent('Shakespeare John', 'Shakespeare Gilbert').
+parent('Arden Mary', 'Shakespeare Gilbert').
+parent('Shakespeare John', 'Shakespeare Joan2').
+parent('Arden Mary', 'Shakespeare Joan2').
+parent('Shakespeare John', 'Shakespeare Anne').
+parent('Arden Mary', 'Shakespeare Anne').
+parent('Shakespeare John', 'Shakespeare Richard Jr.').
+parent('Arden Mary', 'Shakespeare Richard Jr.').
+parent('Shakespeare John', 'Shakespeare Edmund').
+parent('Arden Mary', 'Shakespeare Edmund').
+parent('Shakespeare William', 'Shakespeare Susanna').
+parent('Hathaway Anne', 'Shakespeare Susanna').
+parent('Shakespeare William', 'Shakespeare Hamnet').
+parent('Hathaway Anne', 'Shakespeare Hamnet').
+parent('Shakespeare William', 'Shakespeare Judith').
+parent('Hathaway Anne', 'Shakespeare Judith').
+parent('Shakespeare Richard', 'Shakespeare Henry').
+parent('Webb Abigail', 'Shakespeare Henry').
+parent('Shakespeare Richard', 'Shakespeare John').
+parent('Webb Abigail', 'Shakespeare John').
+parent('Shakespeare Richard', 'Shakespeare Anna').
+parent('Webb Abigail', 'Shakespeare Anna').
+parent('Shakespeare Richard', 'Shakespeare Thomas').
+parent('Webb Abigail', 'Shakespeare Thomas').
+parent('Shakespeare Richard', 'Shakespeare Matthew').
+parent('Webb Abigail', 'Shakespeare Matthew').
+parent('Arden Robert', 'Arden Mary').
+parent('Webbe Agnes', 'Arden Mary').
+parent('Hall John', 'Shakespeare Elizabeth').
+parent('Shakespeare Susanna', 'Shakespeare Elizabeth').
+parent('Quiney Thomas', 'Quiney Shakespeare').
+parent('Shakespeare Judith', 'Quiney Shakespeare').
+parent('Quiney Thomas', 'Quiney Richard').
+parent('Shakespeare Judith', 'Quiney Richard').
+parent('Quiney Thomas', 'Quiney Thomas Jr.').
+parent('Shakespeare Judith', 'Quiney Thomas Jr.').
+parent('Hathaway Richard', 'Hathaway Anne').
+parent('Hathaway Joan', 'Hathaway Anne').
+parent('Shakespeare John Sr.', 'Shakespeare Richard').
+parent('Unknown Joan', 'Shakespeare Richard').
+parent('Hart William', 'Hart William Jr.').
+parent('Shakespeare Joan2', 'Hart William Jr.').
+parent('Hart William', 'Hart Mary').
+parent('Shakespeare Joan2', 'Hart Mary').
+parent('Hart William', 'Hart Thomas').
+parent('Shakespeare Joan2', 'Hart Thomas').
+parent('Hart William', 'Hart Michael').
+parent('Shakespeare Joan2', 'Hart Michael').
